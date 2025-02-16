df880c3ca67daa57b54c7f6a22ce760d7255593c
IsaakTq <87341246+IsaakTq@users.noreply.github.com>
2025-02-15 20:59:39 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+
+function toggleMenu() {
+    const mobileMenu = document.getElementById('mobileMenu');
+    if (mobileMenu.style.display === 'block') {
+        mobileMenu.style.display = 'none';
+    } else {
+        mobileMenu.style.display = 'block';
+    }
+}
