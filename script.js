fd59e831094f0234ee25300de98cab7fd0e15a12
IsaakTq <87341246+IsaakTq@users.noreply.github.com>
2025-02-16 21:28:04 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-// script.js
-
+// Función para mostrar/ocultar el menú en móviles
 function toggleMenu() {
-    const mobileMenu = document.getElementById('mobileMenu');
-    if (mobileMenu.style.display === 'block') {
-        mobileMenu.style.display = 'none';
+    var menu = document.getElementById("mobile-menu");
+    if (menu.classList.contains("w3-hide")) {
+        menu.classList.remove("w3-hide");
+        menu.classList.add("w3-show");
     } else {
-        mobileMenu.style.display = 'block';
+        menu.classList.remove("w3-show");
+        menu.classList.add("w3-hide");
     }
+}
+
+// Función para mostrar/ocultar el menú en móviles
+function toggleMenu() {
+    var menu = document.getElementById("mobile-menu");
+    menu.classList.toggle("w3-show");
 }
